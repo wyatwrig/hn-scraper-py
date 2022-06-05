6a89da853c6501f606657ba51ca98ec48ebea159
Wyatt Wright <wyatwrig@gmail.com>
2022-06-05 13:13:42 -0700

    v1.0 fully functional

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# Hacker News Web Scraper Python
+# Python Web Scraper & Email Generator
  
 This project scrapes the front page of hacker news using python and sends an email of the titles to a users email address.
 
-## Steps
-- Request the page using the requests library
-- Use beautiful soup to pull out the relevant headlines
-- Write and send an email using smtplib, email.mime and datetime.
+![hacker news homepage](./img/hackernews.png) | ![email of links](./img/email.jpg)
 
-## Requirements
-see requirements.txt
+## Usage
+This project depends on creating a google cloud project in order to enable the gmail API and get Oauth client and secret keys. For more details on that process, read this [stackoverflow post.](https://stackoverflow.com/a/72346413/9991592)
+
+To change the email addresse receiving the content list, change the global variable `SENDTO`.
+
+To change the email address sending the content list, add another email address to the test user section in the google cloud project and change the global variable `SENDFROM`.
+
+## Dependencies
+- [Beautiful Soup 4](https://beautiful-soup-4.readthedocs.io/en/latest/)
+- [Requests](https://requests.readthedocs.io/en/latest/)
+- [Yagmail](https://yagmail.readthedocs.io/en/latest/)
+see requirements.txt for full list
+
+## Roadmap/Extensions
+- Error handling
+- Allow users to specify site to be scanned 
+- Allow users to specify which tags and elements to fetch
+- GUI interface
--- hn_scraper.py
+++ hn_scraper.py
@@ -1,68 +1,35 @@
-import os
+"""
+Written by Wyatt Wright
+6/5/2022
+v1.0
+
+This script scrapes hacker news for headlines and generates and sends an email.
+For details on setup and usage see README.md
+"""
+
 import requests
 from bs4 import BeautifulSoup
-import oauth2client
-from oauth2client import client, tools, file
-from apiclient import errors, discovery
-import httplib2
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import yagmail
 import datetime
 
-SCOPES = 'https://www.googleapis.com/auth/gmail.send'
-CLIENT_SECRET_FILE = "C:\\Users\\wyatw\\Desktop\\Projects\\pythonAutomation\\HackerNewsScraper\\client_secret.json"
-APPLICATION_NAME = 'Gmail API Python Send Email'
+SENDFROM = 'anothera690@gmail.com'
+SENDTO = 'wyatwrig@gmail.com'
+
 
 
 def extract_news(url): # function to extract titles
     print('Extracting Hacker News Stories...') # operating message
     cnt = ''
-    cnt += ('<strong>HN Top Stories: <\strong>\n' +
-            '<br>' +
-            '-' * 50 +
-            '<br>') # email heading
+    cnt += ('HN Top Stories: \n' + ('-' * 50) + '\n') # email heading
     response = requests.get(url) # http get using input url
     content = response.content # keeping packet body from server response
     soup = BeautifulSoup(content, 'html.parser') # parsing the packet body with bs4
     for i, tag in enumerate(soup.find_all('a', attrs={'class':'titlelink'})):
         # gets index and tag for all html elements of type td with class = 'title' and y-align = ''
-        cnt += ((str(i+1)+' :: ' + tag.text + "\n" + '<br>') if tag.text != 'More' else '')
+        cnt += ((str(i+1)+' :: ' + tag.text + " " + tag['href'] + "\n") if tag.text != 'More' else '')
+        cnt += '\n'
         # appends to email body the number and text of the headline
     return cnt
-
-def get_credentials():
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-email-send.json')
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print(credentials)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def SendMessage(msgHtml):
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
-    result = SendMessageInternal(service, "me", msgHtml)
-    return result
-
-def SendMessageInternal(service, user_id, message):
-    try:
-        message = (service.users().messages().send(userId=user_id, body=message).execute())
-        print('Message Id: %s' % message['id'])
-        return message
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
-        return "Error"
-    return "OK"
 
 def main():
     now = datetime.datetime.now() # getting the current date
@@ -70,21 +37,16 @@ def main():
     content = '' # placeholder content
     cnt = extract_news('https://news.ycombinator.com/')
     content += cnt
-    content += ('<br>------<br>')
-    content += ('<br><br>End of Message')
+    content += ('-' * 50)
+    content += ('\nEnd of Message')
 
-    to = "wyatwrig@gmail.com"
-    sender = "anothera690@gmail.com"
     subject = 'Top News Stories HN [Auto-Generated]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
-    msg = MIMEMultipart()
 
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = to
-    msg.attach(MIMEText(content, 'html'))
+    print("Sending Email...")
+    yag = yagmail.SMTP(SENDFROM, oauth2_file="./credentials.json") # See README.md for oauth2_file details
+    yag.send(SENDTO, subject, content)
 
-    SendMessage(msg)
-
+    print("Email sent...")
 
 
 if __name__ == "__main__":
